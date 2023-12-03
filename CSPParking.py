2dde9c06a5b670a2d24b1da0a05c119b781342a9
david <tipiraenkewa@gmail.com>
2023-12-03 14:50:31 +0100

    imprimir salida

--- CSPParking.py
+++ CSPParking.py
@@ -1,6 +1,34 @@
 from constraint import Problem, AllDifferentConstraint, InSetConstraint
 from constraint import *
 import sys
+import csv
+import random
+
+def imprimir_soluciones(soluciones, archivo, filas, columnas):
+    # Crear el nombre del archivo de salida
+    archivo_salida = archivo.split('.')[0] + '.csv'
+
+    # Abrir el archivo de salida en modo escritura
+    with open(archivo_salida, 'w', newline='') as f:
+        writer = csv.writer(f)
+
+        # Escribir el número de soluciones en la primera línea
+        writer.writerow(["N. Sol: ", len(soluciones)])
+
+        if len(soluciones) > 2:
+            soluciones = random.sample(soluciones, 2)
+        # Para cada solución, escribir las filas del parking
+        for solucion in soluciones:
+            # Crear una matriz para representar el parking
+            parking = [['-'] * columnas for _ in range(filas)]
+
+            # Rellenar la matriz con los vehículos de la solución
+            for vehiculo, plaza in solucion.items():
+                parking[plaza[0]-1][plaza[1]-1] = vehiculo
+
+            # Escribir la matriz en el archivo
+            for fila in parking:
+                writer.writerow(fila)
 
 def resolver_problema(filas, columnas, plazas_electricas, vehiculos):
     problem = Problem()
@@ -24,48 +52,7 @@ def resolver_problema(filas, columnas, plazas_electricas, vehiculos):
         for vehi2 in vehiculos:
             vehiculo2id = vehi2[0]
             problem.addConstraint(lambda v1, v2: (v1[0]-1!=v2[0] and v1[0]!=1)  or (v1[0]+1!=v2[0] and v1[0]!=filas), (id, vehiculo2id)) 
-    solutions = problem.getSolutions()
-    i = 0
-    for sol in solutions:
-        i += 1
-    print(i)
-
-
-
-"""
-def resolver_problema(filas, columnas, plazas_electricas, vehiculos):
-    problem = Problem()
-
-    # Crear el parking como una lista de tuplas que representan las plazas
-    parking = [(i, j) for i in range(1, filas+1) for j in range(1, columnas+1)]
-
-    # Añadir las variables al problema. Cada vehículo es una variable y las plazas de parking son sus dominios
-    for id, tipo, congelador in vehiculos:
-        problem.addVariable(id, parking)
-
-    # Añadir la restricción de que todos los vehículos deben estar en plazas diferentes
-    problem.addConstraint(AllDifferentConstraint(), [id for id, tipo, congelador in vehiculos])
-    # Añadir las restricciones específicas del problema
-    for id1, tipo1, congelador1 in vehiculos:
-        if tipo1 == 'TSU':
-            for id2, tipo2, congelador2 in vehiculos:
-                if id1 != id2:
-                    # Restricción 4
-                    problem.addConstraint(lambda x, y: x[0] <= y[0] or x[1] != y[1], (id1, id2))
-        if congelador1:
-            # Restricción 3
-            problem.addConstraint(lambda x: x in plazas_electricas, (id1,))
-        for id2, tipo2, congelador2 in vehiculos:
-            for id3, tipo3, congelador3 in vehiculos:
-                if id1 != id2 and id1 != id3 and id2 != id3:
-                    # Restricción 5
-                    problem.addConstraint(lambda x, y, z: x[1] != y[1] or x[1] != z[1], (id1, id2, id3))
-
-    # Resolver el problema
-    soluciones = problem.getSolutions()
-
-    return soluciones
-"""
+    return problem.getSolutions()
 
 def leer_fichero():
     # Comprobar si se ha proporcionado un argumento de línea de comandos
@@ -93,10 +80,8 @@ def leer_fichero():
         vehiculos.append((id, tipo, congelador))
     # Llamar a la función que resuelve el problema
     soluciones = resolver_problema(filas, columnas, plazas_electricas, vehiculos)
+    imprimir_soluciones(soluciones, archivo, filas, columnas)
 
-    # Imprimir las soluciones
-    #    for solucion in soluciones:
-     #   print(solucion)
 
 
 if __name__ == "__main__":
