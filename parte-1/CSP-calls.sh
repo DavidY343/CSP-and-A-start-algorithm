4e5ed1891de3b147c19dae5a7bc1a00c106d7328
Ferjodios <100472111@alumnos.uc3m.es>
2023-12-15 12:34:11 +0100

    Casos de prueba de la primera parte terminado

--- parte-1/CSP-calls.sh
+++ parte-1/CSP-calls.sh
@@ -5,14 +5,14 @@
 python3 CSPParking.py CSP-tests/parking01.dat
 # Un parking pequeño con solo 3 soluciones
 python3 CSPParking.py CSP-tests/parking02.dat
-# Otro caso con 38 soluciones
+# Otro caso con 38 soluciones completo pero mucho más corto que el primero.
 python3 CSPParking.py CSP-tests/parking03.dat
 # Caso de prueba para un parking con una sola fila y múltiples plazas con conexión eléctrica:
 python3 CSPParking.py CSP-tests/parking04.dat
-# Caso de prueba para un parking pequeño con pocas plazas y varios vehículos con y sin congelador:
-python3 CSPParking.py CSP-tests/parking06.dat
+# Caso de prueba para un parking sin plazas eléctricas ni vehículos con congelador:
+python3 CSPParking.py CSP-tests/parking05.dat
 # Caso de prueba con mas ambulancias con congelador que plazas electricas
-python3 CSPParking.py CSP-tests/parking07.dat
+python3 CSPParking.py CSP-tests/parking06.dat
 # Caso de prueba que obliga a infringir la segunda restriccion
-python3 CSPParking.py CSP-tests/parking08.dat
+python3 CSPParking.py CSP-tests/parking07.dat
 echo "Ejecución de casos de prueba completada."
